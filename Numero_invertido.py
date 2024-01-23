7bb0cd0b90318236b06ffe0ce5de4d1a44ef3939
JuanSebastianRey <149455429+JuanSebastianRey@users.noreply.github.com>
2024-01-23 10:19:05 -0500

    Optimizacion del ejercicio 5

--- Numero_invertido.py
+++ Numero_invertido.py
@@ -1,10 +1,16 @@
 # Escriba un programa que pida al usuario un entero de tres dígitos,
 # y entregue el número con los dígitos en orden inverso:
 
-number = int(input('Ingrese numero:\n'))
-i_n = 0
-while number>0:
-    i_n *=10
-    i_n += (number%10)
-    i_n//10
-print(i_n)
+numero = int(input("Ingrese un número de tres dígitos: "))
+
+while numero < 100 or numero > 999:
+   print("El número debe tener exactamente tres dígitos.")
+   numero = int(input("Ingrese un número de tres dígitos: "))
+
+d1 = numero // 100 
+d2 = (numero % 100) // 10 
+d3 = numero % 10 
+
+numero_invertido = d3 * 100 + d2 * 10 + d1
+
+print("El número invertido es:", numero_invertido)
